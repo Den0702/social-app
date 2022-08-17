ce5a99cabe72e4ba73184bbe58e7303bb4a30406
Dzianis Matsiushyn <ellan@tut.by>
2022-08-17 11:21:23 +0200

    Styling for sthe ShowOlder button added

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,7 @@
 import React, { Component } from "react";
 import axios from "axios";
 
+import '../css/Home.css';
 import PostAdd from './PostAdd';
 import Post from './Post';
 import FollowRecommendations from './FollowRecommendations';
