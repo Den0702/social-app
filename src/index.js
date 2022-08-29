aa195e2f58b370ca23a251cd05917c71a5243e9d
Dzianis Matsiushyn <dzmats@gmail.com>
2022-08-29 21:37:52 +0200

    Configuration before deployment

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter} from 'react-router-dom'
+/* import {BrowserRouter} from 'react-router-dom' */
+import { HashRouter } from 'react-router-dom';
 
 import './index.css';
 import './normalize.css';
@@ -8,16 +9,16 @@ import './normalize.css';
 import App from './components/App';
 
 
-/* Infographics */
+/* Setting Infographics */
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
 library.add(fas, far);
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
   ,
   document.getElementById('root')
 );
